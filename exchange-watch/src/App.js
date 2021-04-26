78f9f21200526e59b0cb7e347a0c639b9c4bbe1c
lousteez <louissterrett@gmail.com>
2021-04-26 12:58:09 -0500

    Header, Nav, About, and Dashboard components return text to App.js

--- exchange-watch/src/App.js
+++ exchange-watch/src/App.js
@@ -1,17 +1,19 @@
 import './App.css';
 import React from 'react'
 import About from './components/About'
+import Dashboard from './components/Dashboard'
 import Header from './components/Header'
 import Nav from './components/Nav'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+        <Header />
+        <Nav />
+        <About />
+        <Dashboard />
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ exchange-watch/src/components/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Dashboard(props) {
+    console.log(`Dashboard has been loaded`)
+    return (
+        <div>
+            Dashboard-text
+        </div>
+    );
+}
+
+export default Dashboard;
