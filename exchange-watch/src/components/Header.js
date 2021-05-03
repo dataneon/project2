b441f1396e7737d68af9161ce60d27a7886fe546
lousteez <louissterrett@gmail.com>
2021-05-03 00:09:57 -0500

    updating README.md

--- exchange-watch/src/components/Header.js
+++ exchange-watch/src/components/Header.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import { Helmet } from 'react-helmet'
 
 function Header() {
     return (
         <div id="headerReturn">
+            <Helmet>
+            <title>Exchange Watch</title>
+            </Helmet>
             <h1>Exchange Watch</h1>
         </div>
     );
